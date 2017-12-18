bb4cab94a0bc0db0214eab2b2469b40de5ac9a07
divine <divinecutler@gmail.com>
2017-12-18 09:35:24 +0530

    updated

---
+++ siteboilerplate_withslider1/src/assets/body/SliderContent.vue
@@ -0,0 +1,87 @@
+<template>
+     <transition-group  id="slidercontent" mode="out-in" tag="div" name="fade">
+        <div class="imagerender" v-for="(imgContent,index) in  imgContents" :key="index" v-if="index == counter">
+            <img :src="imgContent.path" alt="">
+        </div>
+     </transition-group>
+</template>
+
+<script>
+export default {
+  props:["imgContents","counter"]
+}
+</script>
+
+<style>
+/* #slidercontent{
+  
+  height: inherit;
+  width: auto;
+  display: inline-block;
+}
+ */
+#slidercontent{
+  overflow: hidden;
+}
+#slidercontent .imagerender, #slidercontent .imagerender img{
+  height: inherit;
+    width: auto;
+  display: inline-block;
+}
+#slidercontent .imagerender img{
+  width: 100%;
+}
+/* .fade-leave-active,
+.fade-enter-active {
+  transition: 1s;
+}
+.fade-enter-active {
+  transform: translate(100%, 0);
+}
+.fade-leave-active {
+  transform: translate(-100%, 0);
+} */
+
+ .fade-enter-active{
+    animation-name: fade-enter;
+    animation-iteration-count: 1;
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+    animation-timing-function: ease-in;
+}
+.fade-move{
+  transition: transform 1s;
+}
+.fade-leave-active{
+    
+    animation-name: fade-leave;
+    animation-iteration-count: 1;
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+    animation-timing-function: ease-in;
+    position: absolute;
+}
+@keyframes fade-enter {
+    from{
+        
+        transform: translate(100%,0px); 
+        
+    }
+    to{
+         
+          transform: translate(0px,0px); 
+    }
+}   
+@keyframes fade-leave {
+    from{
+       
+         transform: translate(0px,0px); 
+    }
+    to{
+       
+          transform: translate(-100%,0px); 
+    }
+}
+
+
+</style>
