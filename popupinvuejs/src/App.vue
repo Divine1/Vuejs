285327d0635dbb20aaf1341996ca404ed1e55512
Divine C <Divine_C@ad.infosys.com>
2017-12-27 15:12:42 +0530

    simple popup implementation

---
+++ popupinvuejs/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>Main page</div>
+    <button @click="openpopup">Open popup</button>
+    <popup :popupData="popupData" ></popup>
+  </div>
+</template>
+<script>
+  import Popup from'./Popup.vue';
+  export default {
+    components:{
+      "popup" : Popup
+    },
+    data() {
+      return {
+        popupData : {
+          "header" : "My popup",
+          "body" : "hello world",
+          "footer" : "divine inc.",
+          "display" : "none"
+        }
+      }
+    },
+    methods : {
+      openpopup(){
+        this.popupData.display = "block";
+      }
+    },
+    mounted(){
+      this.popupData.display = "block";
+    }
+  }
+</script>
