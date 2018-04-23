03c61dea9ba005c26c81e0d85ffa9d72d235be52
divine <divinecutler@gmail.com>
2018-04-24 00:00:24 +0530

    update

---
+++ try-notify/src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueNotifications from 'vue-notifications'
+import miniToastr from 'mini-toastr'
+
+const toastTypes = {
+  success: 'success',
+  error: 'error',
+  info: 'info',
+  warn: 'warn'
+}
+
+const iconTypes =  {
+'success' : "fas fa-user"
+}
+
+miniToastr.init({types: toastTypes,icons: iconTypes})
+
+function toast ({title, message, type, timeout, cb}) {
+  return miniToastr[type](message, title, timeout, cb)
+}
+
+const options = {
+  success: toast,
+  error: toast,
+  info: toast,
+  warn: toast
+}
+Vue.use(VueNotifications, options)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
